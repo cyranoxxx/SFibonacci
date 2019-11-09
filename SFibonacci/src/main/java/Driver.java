8ad0330815998e4918dcbbd0034b650c39c7a29e
cyranoxxx <ggolhan@gmail.com>
2019-11-09 14:34:29 +0300

    Update Driver.java

--- SFibonacci/src/main/java/Driver.java
+++ SFibonacci/src/main/java/Driver.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	Scanner scan = new Scanner(System.in);
         System.out.println("Bir Sayi giriniz?");
 
         int number = scan.nextInt();
